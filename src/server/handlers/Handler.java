5aa90621320c5a389bbde29c312fd77b42966b12
Matt Crowder <mcrowder65@gmail.com>
2017-02-16 09:01:39 -0700

    don't use read file

--- src/server/handlers/Handler.java
+++ src/server/handlers/Handler.java
@@ -1,7 +1,5 @@
 package server.handlers;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -25,32 +23,31 @@ public class Handler implements HttpHandler {
 		exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
 		exchange.getResponseBody().write("Your java server is running.".getBytes());
 		exchange.getResponseBody().close();
-		readFile("../../client/components/walker/walker.html");
 	}
 
-	private void readFile(String filename) {
-		BufferedReader br = null;
-		FileReader fr = null;
-
-		try {
-
-			fr = new FileReader(filename);
-			br = new BufferedReader(fr);
-
-			String sCurrentLine;
-
-			br = new BufferedReader(new FileReader(filename));
-
-			while ((sCurrentLine = br.readLine()) != null) {
-				System.out.println(sCurrentLine);
-			}
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-
-		}
-
-	}
+	// private void readFile(String filename) {
+	// BufferedReader br = null;
+	// FileReader fr = null;
+	//
+	// try {
+	//
+	// fr = new FileReader(filename);
+	// br = new BufferedReader(fr);
+	//
+	// String sCurrentLine;
+	//
+	// br = new BufferedReader(new FileReader(filename));
+	//
+	// while ((sCurrentLine = br.readLine()) != null) {
+	// System.out.println(sCurrentLine);
+	// }
+	//
+	// } catch (IOException e) {
+	//
+	// e.printStackTrace();
+	//
+	// }
+	//
+	// }
 
 }
