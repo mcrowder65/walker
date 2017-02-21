b1e89584b2597c054ff387127bdeacb59e19baf8
Eric <soreric94@gmail.com>
2017-02-21 13:11:09 -0700

    jijfiejwfiew

--- src/server/APITools.java
+++ src/server/APITools.java
@@ -1,11 +1,16 @@
 package server;
 
 import org.json.JSONArray;
-
+import org.json.JSONObject;
 public class APITools {
 
 	public static String GetOverviewPolyline(String apiJSONResponse)
 	{
-		org.json
+		JSONObject rootObj = new JSONObject(apiJSONResponse);
+		JSONArray routes = rootObj.getJSONArray("routes");
+		JSONObject primaryRoute = routes.getJSONObject(0);
+		JSONObject overviewPolyline = primaryRoute.getJSONObject("overview_polyline");
+		String encPolyline = overviewPolyline.getString("points");
+		return encPolyline;
 	}
 }
