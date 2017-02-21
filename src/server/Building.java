e78559db83524cb1991fdf39b3c6f12e2b65c8d9
Matt Crowder <mcrowder65@gmail.com>
2017-02-21 12:50:42 -0700

    tons of jars

--- src/server/Building.java
+++ src/server/Building.java
@@ -1,5 +1,27 @@
 package server;
 
-public class Building {
+import com.google.gson.Gson;
 
+public class Building {
+	public String name;
+	transient private Gson g;
+
+	@Override
+	public String toString() {
+		return "Building [name=" + name + "]";
+	}
+
+	public Building(String name) {
+		g = new Gson();
+	}
+
+	public Building() {
+		g = new Gson();
+	}
+
+	public String toJson() {
+
+		String jsonString = g.toJson(this);
+		return jsonString;
+	}
 }
--- src/server/handlers/Handler.java
+++ src/server/handlers/Handler.java
@@ -1,15 +1,28 @@
 package server.handlers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import server.Building;
 import sun.net.www.protocol.http.HttpURLConnection;
 
 public class Handler implements HttpHandler {
+	private List<String> buildings;
+	private Gson g;
 
 	public Handler() {
+		buildings = new ArrayList<>();
+		g = new Gson();
 	}
 
 	/**
@@ -18,36 +31,37 @@ public class Handler implements HttpHandler {
 	 */
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
+		final FirebaseDatabase database = FirebaseDatabase.getInstance();
+		DatabaseReference ref = database.getReference("buildings");
+		ref.addValueEventListener(new ValueEventListener() {
+
+			@Override
+			public void onCancelled(DatabaseError arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				// TODO Auto-generated method stub
+				System.out.println("onDataChange");
+				System.out.println(dataSnapshot.getValue());
+				String dataSnapshotStr = (String) dataSnapshot.getValue();
+				for (DataSnapshot child : dataSnapshot.getChildren()) {
+					System.out.println(child);
+					Building building = dataSnapshot.getValue(Building.class);
+				}
+
+				System.out.println("hello");
+
+			}
+		});
 
 		exchange.getResponseHeaders().add("Content-type", "application/json");
 		exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
-		exchange.getResponseBody().write("Your java server is running.".getBytes());
+		exchange.getResponseBody().write("your java server is running".getBytes());
 		exchange.getResponseBody().close();
-	}
 
-	// private void readFile(String filename) {
-	// BufferedReader br = null;
-	// FileReader fr = null;
-	//
-	// try {
-	//
-	// fr = new FileReader(filename);
-	// br = new BufferedReader(fr);
-	//
-	// String sCurrentLine;
-	//
-	// br = new BufferedReader(new FileReader(filename));
-	//
-	// while ((sCurrentLine = br.readLine()) != null) {
-	// System.out.println(sCurrentLine);
-	// }
-	//
-	// } catch (IOException e) {
-	//
-	// e.printStackTrace();
-	//
-	// }
-	//
-	// }
+	}
 
 }
