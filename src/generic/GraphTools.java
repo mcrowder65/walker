fccde5bc1fcda6f92c5d4746ed66041acb19bb7d
bbarney7 <brennen.barney@gmail.com>
2017-02-22 12:09:31 -0700

    updated graph tools

--- src/generic/GraphTools.java
+++ src/generic/GraphTools.java
@@ -2,4 +2,32 @@ package generic;
 
 public class GraphTools {
 
+	private double[][] distance;
+	private double[][] elevation;
+
+	public GraphTools(double[][] distance, double[][] elevation) {
+		this.distance = distance;
+		this.elevation = elevation;
+	}
+
+	public GraphTools() {
+
+	}
+
+	public double[][] getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double[][] distance) {
+		this.distance = distance;
+	}
+
+	public double[][] getElevation() {
+		return elevation;
+	}
+
+	public void setElevation(double[][] elevation) {
+		this.elevation = elevation;
+	}
+
 }
