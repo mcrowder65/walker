1670b01e81dbc2b1ab941618b76b60aeb8a6e20c
Matt Crowder <mcrowder65@gmail.com>
2017-02-23 15:51:20 -0700

    stuff

--- src/client/prendus-learning-platform/app/client/components/prendus-router/prendus-router.ts
+++ src/client/prendus-learning-platform/app/client/components/prendus-router/prendus-router.ts
@@ -12,66 +12,15 @@ class PrendusRouter {
 
   beforeRegister() {
     this.is =  "prendus-router";
-
-    this.observers = [
-      '_routeChanged(route.*)'
-    ];
-
   }
-  async _routeChanged(routeObject: any): Promise<void> {
-    if(!this.username || !this.loggedIn) {
-      // Call default action to determine if the user is logged in, since
-      // this route change function is called before the first mapStateToThis.
-      Actions.defaultAction(this);
-      await Actions.checkUserAuth(this);
-    }
-    const route: string = routeObject.value.path;
-    switch(route) {
 
-      case '/': {
-        if(this.loggedIn === 'true') {
-          UtilitiesService.importElement(this, 'components/prendus-homepage/prendus-homepage.html', 'homepage');
-        } else {
-          UtilitiesService.importElement(this, 'components/prendus-landing/prendus-landing.html', 'landing');
-        }
-
-        break;
-      }
-
-      case '/login': {
-        UtilitiesService.importElement(this, 'components/prendus-login/prendus-login.html', 'login');
-        break;
-      }
-
-      case '/signup': {
-        UtilitiesService.importElement(this, 'components/prendus-create-account/prendus-create-account.html', 'create-account');
-        break;
-      }
-
-      case '/profile': {
-        UtilitiesService.importElement(this, 'components/prendus-profile/prendus-profile.html', 'profile');
-        break;
-      }
-
-      case '/privacy-policy': {
-        UtilitiesService.importElement(this, 'components/prendus-privacy-policy/prendus-privacy-policy.html', 'privacy-policy');
-        break;
-      }
-
-      case '/terms-of-service': {
-        UtilitiesService.importElement(this, 'components/prendus-terms-of-service/prendus-terms-of-service.html', 'terms-of-service');
-        break;
-      }
-
-      default: break;
-    }
-
+  ready(): void {
+    Actions.defaultAction(this);
   }
 
   mapStateToThis(e: StatechangeEvent): void {
       const state = e.detail.state;
-      this.username = state.currentUser.metaData.email;
-      this.loggedIn = this.username ? 'true' : 'false';
+      this.loggedIn = 'false';
       this.mainViewToShow = state.mainViewToShow;
   }
 }
