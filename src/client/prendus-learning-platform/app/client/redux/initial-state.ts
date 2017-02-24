e21ba5c10986c756fabd35a57b4d951f15f1fdd2
Matt Crowder <mcrowder65@gmail.com>
2017-02-24 08:23:53 -0700

    deleting some more

--- src/client/prendus-learning-platform/app/client/redux/initial-state.ts
+++ src/client/prendus-learning-platform/app/client/redux/initial-state.ts
@@ -3,48 +3,4 @@ import {State} from '../typings/state';
 
 export const InitialState: State = {
     mainViewToShow: 'routes',
-    concepts: {
-    },
-    currentConcept: {
-    },
-    courses: [],
-    courseViewCurrentCourse: {},
-    editCourseConcepts: {},
-    viewCourseConcepts: {},
-    editConceptVideos: {},
-    viewConceptVideos: {},
-    currentConceptVideoId: '',
-    currentConceptVideoTitle: '',
-    currentConceptVideoUrl: '',
-    currentUser: {
-        authorizedQuestions: {},
-        authorizedQuizzes: {},
-        metaData: {
-            email: '',
-            firstName: '',
-            lastName: '',
-            institution: '',
-            uid: ''
-        },
-        starredCourses: {},
-        sharedWithMeCourses: {},
-        sharedWithMeConcepts: {},
-        sharedWithMeVideos: {},
-        sharedWithMeQuizzes: {}
-    },
-    userQuestionIds: [],
-    publicQuestionIds: [],
-    quizQuestionIds: [],
-    quizQuestionSettings: {},
-    currentEditQuizId: '',
-    editConceptQuizzes: {},
-    viewConceptQuizzes: {},
-    currentEditConceptId: '',
-    publicCourses: [],
-    starredCourses: [],
-    sharedCourses: [],
-    courseCollaboratorEmails: {},
-    conceptCollaboratorEmails: {},
-    videoCollaboratorEmails: {},
-    quizCollaboratorEmails: {}
 };
