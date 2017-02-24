466307b62435070a1198a9d0d1fa9a82b4930a3c
bbarney7 <brennen.barney@gmail.com>
2017-02-24 13:05:24 -0700

    starting dijkstras

--- src/generic/GraphTools.java
+++ src/generic/GraphTools.java
@@ -9,20 +9,22 @@ import server.Node;
 
 public class GraphTools {
 
-	public static Node[] CreateNodesFromPolyline(String[] polylinePieces, double density)
-	{
+	public static Node[] CreateNodesFromPolyline(String[] polylinePieces, double density) {
 		ArrayList<LatLng> pivots = new ArrayList<LatLng>();
-		for (String poly : polylinePieces)
-		{
+		for (String poly : polylinePieces) {
 			pivots.addAll(PolyUtil.decode(poly));
 		}
 		Node[] nodes = new Node[pivots.size()];
-		for (int n = 0; n < pivots.size(); n++)
-		{
+		for (int n = 0; n < pivots.size(); n++) {
 			nodes[n] = new Node(pivots.get(n).longitude, pivots.get(n).latitude, null);
 		}
 		return nodes;
-			
+
+	}
+
+	public void dijkstra(int startNodeIndex, Graph g) {
+		List<Double> distance = new ArrayList();
+
 	}
 
 }
