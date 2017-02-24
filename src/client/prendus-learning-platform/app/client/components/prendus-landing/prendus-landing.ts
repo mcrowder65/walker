493fcbd41976cb9996362f450e153b7a43781e0c
Matt Crowder <mcrowder65@gmail.com>
2017-02-24 06:53:30 -0700

    google maps u suck

--- src/client/prendus-learning-platform/app/client/components/prendus-landing/prendus-landing.ts
+++ src/client/prendus-learning-platform/app/client/components/prendus-landing/prendus-landing.ts
@@ -2,9 +2,13 @@ import {Actions} from '../../redux/actions';
 
 class PrendusLanding {
   public is: string;
+  public apiKey: string;
 
-  beforeRegister() {
+  beforeRegister(): void {
     this.is = 'prendus-landing';
+  }
+  ready(): void {
+    this.apiKey = 'AIzaSyB7zQeZeCZFWzwupwYjbioQYldZkdF3oPk';
   }
 }
 Polymer(PrendusLanding);
