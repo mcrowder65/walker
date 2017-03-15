d111122ac931dcbf619e272607a0d28d75f419d2
Eric <soreric94@gmail.com>
2017-03-15 12:11:17 -0600

    config

--- src/generic/Config.java
+++ src/generic/Config.java
@@ -7,9 +7,10 @@ public class Config {
 	public static final String DIRECTIONS_KEY = "AIzaSyB7zQeZeCZFWzwupwYjbioQYldZkdF3oPk";
 	public static final String STATICMAP_KEY = "AIzaSyBrlZw4DeO4UtdBQDmSEM9rD6gSkpF3F1g";
 	public static final String ELEVATION_KEY = "AIzaSyCrDlO2Sy--Cwg8P5LSsRKbHaMZK3C9GHc";
-	public static final int MAPS_BACKGROUND_RGB = -1776417;
-	public static final int MAPS_GRASS_RGB = -3479901;
-	public static final int MAPS_NORMALPATH_RGB = -1;
+	public static final int MAPS_BACKGROUND_RGB = -1776417; //TAN
+	public static final int MAPS_GRASS_RGB = -3479901; //GREEN
+	public static final int MAPS_NORMALPATH_RGB = -1; //WHITE
+	public static final int MAPS_BUILDING_RGB = -65536; //RED
 	public static final int FILLCOLOR_PIXELDIM_TOLERANCE = 7;
 	public static final int FILLCOLOR_MODE_RADIUS = 2;
 	public static final int FILLCOLOR_RGB_TOLERANCE = 3;
