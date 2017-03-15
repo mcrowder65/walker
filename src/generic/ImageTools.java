d52b75a1e953f7ff8a6b34a7415cee4f0973b592
bbarney7 <brennen.barney@gmail.com>
2017-03-15 12:21:07 -0600

    push

--- src/generic/ImageTools.java
+++ src/generic/ImageTools.java
@@ -40,79 +40,77 @@ public class ImageTools {
 		int y1 = (int) startPnt.y;
 		int x2 = (int) endPnt.x;
 		int y2 = (int) endPnt.y;
-		
-		List<int>xVals
 
 		int cx, cy, ix, iy, dx, dy, ddx = x2 - x1, ddy = y2 - y1;
 
-		getNumWhitePixles(x2, y2, img);
-
-		if (ddx == 0) { // vertical line special case
-			if (ddy > 0) {
-				cy = y1;
-				do
-					Tools.setImageRGB(img, x1, cy++, lineColor);
-				while (cy <= y2);
-				return;
-			} else {
-				cy = y2;
-				do
-					Tools.setImageRGB(img, x1, cy++, lineColor);
-				while (cy <= y1);
-				return;
-			}
-		}
-		if (ddy == 0) { // horizontal line special case
-			if (ddx > 0) {
-				cx = x1;
-				do
-					Tools.setImageRGB(img, cx, y1, lineColor);
-				while (++cx <= x2);
-				return;
-			} else {
-				cx = x2;
-				do
-					Tools.setImageRGB(img, cx, y1, lineColor);
-				while (++cx <= x1);
-				return;
-			}
-		}
-		if (ddy < 0) {
-			iy = -1;
-			ddy = -ddy;
-		} // pointing up
-		else
-			iy = 1;
-		if (ddx < 0) {
-			ix = -1;
-			ddx = -ddx;
-		} // pointing left
-		else
-			ix = 1;
-		dx = dy = ddx * ddy;
-		cy = y1;
-		cx = x1;
-		if (ddx < ddy) { // < 45 degrees, a tall line
-			do {
-				dx -= ddy;
-				do {
-					Tools.setImageRGB(img, cx, cy, lineColor);
-					cy += iy;
-					dy -= ddx;
-				} while (dy >= dx);
-				cx += ix;
-			} while (dx > 0);
-		} else { // >= 45 degrees, a wide line
-			do {
-				dy -= ddx;
-				do {
-					Tools.setImageRGB(img, cx, cy, lineColor);
-					cx += ix;
-					dx -= ddy;
-				} while (dx >= dy);
-				cy += iy;
-			} while (dy > 0);
-		}
+		// getNumWhitePixles(x2, y2, img);
+		//
+		// if (ddx == 0) { // vertical line special case
+		// if (ddy > 0) {
+		// cy = y1;
+		// do
+		// Tools.setImageRGB(img, x1, cy++, lineColor);
+		// while (cy <= y2);
+		// return;
+		// } else {
+		// cy = y2;
+		// do
+		// Tools.setImageRGB(img, x1, cy++, lineColor);
+		// while (cy <= y1);
+		// return;
+		// }
+		// }
+		// if (ddy == 0) { // horizontal line special case
+		// if (ddx > 0) {
+		// cx = x1;
+		// do
+		// Tools.setImageRGB(img, cx, y1, lineColor);
+		// while (++cx <= x2);
+		// return;
+		// } else {
+		// cx = x2;
+		// do
+		// Tools.setImageRGB(img, cx, y1, lineColor);
+		// while (++cx <= x1);
+		// return;
+		// }
+		// }
+		// if (ddy < 0) {
+		// iy = -1;
+		// ddy = -ddy;
+		// } // pointing up
+		// else
+		// iy = 1;
+		// if (ddx < 0) {
+		// ix = -1;
+		// ddx = -ddx;
+		// } // pointing left
+		// else
+		// ix = 1;
+		// dx = dy = ddx * ddy;
+		// cy = y1;
+		// cx = x1;
+		// if (ddx < ddy) { // < 45 degrees, a tall line
+		// do {
+		// dx -= ddy;
+		// do {
+		// Tools.setImageRGB(img, cx, cy, lineColor);
+		// cy += iy;
+		// dy -= ddx;
+		// } while (dy >= dx);
+		// cx += ix;
+		// } while (dx > 0);
+		// } else { // >= 45 degrees, a wide line
+		// do {
+		// dy -= ddx;
+		// do {
+		// Tools.setImageRGB(img, cx, cy, lineColor);
+		// cx += ix;
+		// dx -= ddy;
+		// } while (dx >= dy);
+		// cy += iy;
+		// } while (dy > 0);
+		// }
 
 		// int rgbStart = img.getRGB(x1, y1);
 		// int rgbEnd = img.getRGB(x2, y2);
--- src/generic/UserPrefs.java
+++ src/generic/UserPrefs.java
@@ -1,19 +1,24 @@
 package generic;
 
 /**
- * Each weight is a double from 0 to 1, 0 being the least important and 1 being the most.
- * i.e. if elevation weight were 1, then choose a long path as long as it means elevation does not change much.
+ * Each weight is a double from 0 to 1, 0 being the least important and 1 being
+ * the most. i.e. if elevation weight were 1, then choose a long path as long as
+ * it means elevation does not change much.
  */
 public class UserPrefs {
 
-	private double distanceWeight;
+	private boolean distance;
+	private boolean building;
+	private boolean parkinglot;
+	private boolean elevator;
+	private boolean wilderness;
 	private double elevationWeight;
-	private double buildingWeight;
-	private double parkinglotWeight;
 	private double wildernessWeight;
 	private double stairsWeight;
-	private double elevatorWeight;
-	public UserPrefs() {}
+
+	public UserPrefs() {
+	}
+
 	public UserPrefs(double distanceWeight, double elevationWeight, double buildingWeight, double parkinglotWeight,
 			double wildernessWeight, double stairsWeight, double elevatorWeight) {
 		super();
@@ -25,8 +30,9 @@ public class UserPrefs {
 		this.stairsWeight = stairsWeight;
 		this.elevatorWeight = elevatorWeight;
 	}
-	public static UserPrefs DISTANCE_ONLY = new UserPrefs(1,0,0,0,0,0,0);
-	
+
+	public static UserPrefs DISTANCE_ONLY = new UserPrefs(1, 0, 0, 0, 0, 0, 0);
+
 	private boolean validateWeight(double weight) {
 		if (weight >= 0 && weight <= 1)
 			return true;
@@ -34,67 +40,80 @@ public class UserPrefs {
 			System.err.println("ERROR: Weight must be between 0 and 1. Ignored.");
 		return false;
 	}
-	
+
 	public double getDistanceWeight() {
 		return distanceWeight;
 	}
+
 	public void setDistanceWeight(double distanceWeight) {
 		if (!validateWeight(distanceWeight))
 			return;
 		this.distanceWeight = distanceWeight;
 	}
+
 	public double getElevationWeight() {
-		
+
 		return elevationWeight;
 	}
+
 	public void setElevationWeight(double elevationWeight) {
 		if (!validateWeight(elevationWeight))
 			return;
 		this.elevationWeight = elevationWeight;
 	}
+
 	public double getBuildingWeight() {
 		return buildingWeight;
 	}
+
 	public void setBuildingWeight(double buildingWeight) {
 		if (!validateWeight(buildingWeight))
 			return;
-		
+
 		this.buildingWeight = buildingWeight;
 	}
+
 	public double getParkinglotWeight() {
 		return parkinglotWeight;
 	}
+
 	public void setParkinglotWeight(double parkinglotWeight) {
 		if (!validateWeight(parkinglotWeight))
 			return;
-		
+
 		this.parkinglotWeight = parkinglotWeight;
 	}
+
 	public double getWildernessWeight() {
 		return wildernessWeight;
 	}
+
 	public void setWildernessWeight(double wildernessWeight) {
 		if (!validateWeight(wildernessWeight))
 			return;
-		
+
 		this.wildernessWeight = wildernessWeight;
 	}
+
 	public double getStairsWeight() {
 		return stairsWeight;
 	}
+
 	public void setStairsWeight(double stairsWeight) {
 		if (!validateWeight(stairsWeight))
 			return;
-		
+
 		this.stairsWeight = stairsWeight;
 	}
+
 	public double getElevatorWeight() {
 		return elevatorWeight;
 	}
+
 	public void setElevatorWeight(double elevatorWeight) {
 		if (!validateWeight(elevatorWeight))
 			return;
-		
+
 		this.elevatorWeight = elevatorWeight;
 	}
 }
