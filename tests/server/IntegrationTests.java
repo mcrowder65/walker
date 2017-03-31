c46d176100e9037f98e440b148e0053366afb22c
Eric <soreric94@gmail.com>
2017-03-31 14:32:01 -0600

    with integration test

--- tests/server/IntegrationTests.java
+++ tests/server/IntegrationTests.java
@@ -4,13 +4,19 @@ import static org.junit.Assert.*;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
 import java.util.List;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 import generic.Config;
 import generic.Graph;
 import generic.GraphTools;
+import generic.ImageTools;
 import generic.Node;
 import generic.Tools;
 import generic.objects.Building;
@@ -20,6 +26,13 @@ import server.handlers.building.GetBuildingsHandler;
 
 public class IntegrationTests {
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		FirebaseOptions options = new FirebaseOptions.Builder().setServiceAccount(new FileInputStream("key.json"))
+				.setDatabaseUrl("https://walker-73119.firebaseio.com/").build();
+		FirebaseApp.initializeApp(options);
+	}
+	
 	@Test
 	public void createCampusGraphTest() {
 		//Generates the tiled image of the campus, creates the graph, and stores it in firebase
@@ -34,16 +47,21 @@ public class IntegrationTests {
 			img = APITools.GetTiledImage(southwest, northeast, zoom, false);
 
 		
+		List<Building> buildings = BuildingDAO.getAll();
+		img = ImageTools.fillBuildings(img, buildings, southwest, northeast);
+		
+		
 		List<Node> nodes = generic.GraphTools.GenerateUniformNodes(6, southwest, northeast);
 		GraphTools.RemoveBuildingNodes(nodes, img, southwest, northeast);
 		
-		BuildingDAO bDAO = new BuildingDAO();
-		List<Building> buildings = bDAO.getAll();
+
+		
+	//	Tools.WriteImage(img, "testImages/fillCampusTest.png");
 		
 		
 		Graph g = new Graph(null, null, nodes);
 		g.setLimitedDistancesFromNodes(img, southwest, northeast);
-		//g.generateLimitedMatrix(img, southwest, northeast);
+		
 		GraphTools.WriteGraphToImage(img, g, Color.BLUE, 1, southwest, northeast);
 		img = Tools.ClipLogo(img);
 		Tools.WriteImage(img, "testImages/fullCampus.png");
