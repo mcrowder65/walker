603f54bed3185d58a33ff2c00e4159a595193a54
Matt Crowder <mcrowder65@gmail.com>
2017-04-12 12:32:20 -0600

    log

--- src/server/handlers/travel/TravelHandler.java
+++ src/server/handlers/travel/TravelHandler.java
@@ -92,7 +92,8 @@ public class TravelHandler extends WalkerHandler {
 			starPath.add(0, endNode);
 			for (int i = 0; i < starPath.size(); i++) {
 				Node n = g.getFromIndex(starPath.get(i));
-				Marker m = new Marker(n.getPosition().latitude - Config.LAT_BIAS, n.getPosition().longitude - Config.LON_BIAS);
+				Marker m = new Marker(n.getPosition().latitude - Config.LAT_BIAS,
+						n.getPosition().longitude - Config.LON_BIAS);
 				markers.add(m);
 			}
 			// GraphTools.WriteAStarPathToImage(img, g, starPath, southwest,
@@ -104,6 +105,9 @@ public class TravelHandler extends WalkerHandler {
 			System.err.println("endNode: " + endNode);
 			System.err.println("start: " + start);
 			System.err.println("end: " + end);
+			System.err.println("startMarker: " + startMarker);
+			System.err.println("endMarker: " + endMarker);
+			System.err.println("userpreferences: " + up);
 			throw e;
 		}
 
